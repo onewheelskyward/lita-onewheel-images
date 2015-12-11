32666cb847113c6e24b00738967b97a69d144fe2
Andrew Kreps <andrew.kreps@gmail.com>
2015-12-10 16:36:14 -0800

    0.0.0 yo!

--- lib/lita/handlers/onewheel_images.rb
+++ lib/lita/handlers/onewheel_images.rb
@@ -1,10 +1,25 @@
+require 'httparty'
+require 'json'
+
 module Lita
   module Handlers
     class OnewheelImages < Handler
-      # insert handler code here
-      route /image/, :image, command: true
+      config :custom_search_engine_id
+      config :google_api_key
+
+      route /image\s+(.*)$/, :image, command: true
+
       def image(response)
-        response.reply 'Image!'
+        query = response.matches[0][0]
+        search_result = get_results query
+        puts search_result.inspect
+        response.reply search_result['items'][0]['link']
+      end
+
+      def get_results(query)
+        puts "Searching for #{query}"
+        response = HTTParty.get "https://www.googleapis.com/customsearch/v1?q=#{query}&cx=#{config.custom_search_engine_id}&num=10&searchType=image&key=#{config.google_api_key}"
+        JSON.parse response.body
       end
 
       Lita.register_handler(self)
--- spec/lita/handlers/onewheel_images_spec.rb
+++ spec/lita/handlers/onewheel_images_spec.rb
@@ -1,4 +1,22 @@
 require 'spec_helper'
 
 describe Lita::Handlers::OnewheelImages, lita_handler: true do
+
+  before(:each) do
+    mock_result_json = File.open('spec/fixtures/mock_result.json').read
+    allow(Lita::Handlers::OnewheelImages).to receive(:get_results).and_return(mock_result_json)
+
+    registry.configure do |config|
+      config.handlers.onewheel_images.custom_search_engine_id = ''
+      config.handlers.onewheel_images.google_api_key = ''
+    end
+  end
+
+  it { is_expected.to route_command('image') }
+
+  it 'does neat imagey things' do
+    # mock gcse response
+    send_command 'image yo'
+
+  end
 end
